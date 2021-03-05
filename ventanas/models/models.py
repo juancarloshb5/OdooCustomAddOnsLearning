fdd28a189c4db57474af55c66327f3bafd3d091f
juancarloshb5 <46902413+juancarloshb5@users.noreply.github.com>
2021-03-05 17:28:39 -0400

    ViewsSistemasVentanas

--- ventanas/models/models.py
+++ ventanas/models/models.py
@@ -1,26 +1,28 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models,fields
+from odoo import models,fields,api
 
 class ventana(models.Model):
     _name = "ventanas.ventana"
     _descripcion = "modelo de ventana"
 
-    name = fields.Char("Id")
+    name = fields.Char("Nombre")
     ubicacion = fields.Char("ubicacion")
     precio = fields.Float("Precio")
+    medida = fields.Selection([("pulg","Pulgadas"),("mm", "Milimetros")])
+    ancho = fields.Integer("Ancho")
+    alto = fields.Integer("Alto")
+    area = fields.Float("Area", compute="_area_pc", store=True)
+    sistema = fields.Many2one("ventanas.sistema", "Sistema", required=False)
+
+    @api.depends('ancho','alto')
+    def _area_pc(self):
+        self.area = self.ancho * self.alto / 1000000
 
 
-# class ventanas(models.Model):
-#     _name = 'ventanas.ventanas'
-#     _description = 'ventanas.ventanas'
+class Sistema(models.Model):
+    _name ="ventanas.sistema"
+    _description = "Sistemas"
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    name= fields.Char("Codigo")
+    description = fields.Char("Descripcion")
